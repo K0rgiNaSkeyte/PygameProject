e29a27a358dbe29ec8a75c9ffd15ea129ee38100
Korgi_na_skeyte <vereshagindanila2007@gmail.com>
2025-01-05 02:29:24 +0300

    Add grabbers.py

---
+++ utils/grabbers.py
@@ -0,0 +1,13 @@
+class Getters:
+    def get_cur_coords(self):
+        x1, y1 = event.pos()
+        return x1, y1
+
+    def get_player_coords(self):
+        pass
+
+    def get_map(self):
+        pass
+
+    def get_enemy_coords(self):
+        pass
