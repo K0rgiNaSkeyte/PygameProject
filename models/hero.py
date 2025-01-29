dc8154b411e84fe1b8394933593ede42ab39095f
Korgi_na_skeyte <vereshagindanila2007@gmail.com>
2025-01-30 01:31:45 +0300

    edit hero and Level1

--- models/hero.py
+++ models/hero.py
@@ -3,4 +3,11 @@ from utils.game_object import GameObject
 
 
 class Hero(GameObject):
-    pass
+    def __init__(self, position):
+        self_x, self.y = position
+
+    def get_pos(self):
+        return self.x, self.y
+
+    def render(self):
+        pass
