fc8d718f14071fc9050c092cd0b79b0655854258
Korgi_na_skeyte <vereshagindanila2007@gmail.com>
2025-02-03 21:28:19 +0300

    Add save

--- database/db_manager.py
+++ database/db_manager.py
@@ -30,10 +30,22 @@ class DatabaseManager:
             )
         ''')
 
+        cur.execute('''
+            CREATE TABLE IF NOT EXISTS enemy_data (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                enemy_type TEXT NOT NULL,
+                defeated BOOLEAN NOT NULL,
+                position_x INTEGER NOT NULL,
+                position_y INTEGER NOT NULL,
+                save_name TEXT NOT NULL,
+                FOREIGN KEY (save_name) REFERENCES player_data (save_name)
+            )
+        ''')
+
         conn.commit()
         conn.close()
 
-    def save_game(self, x_pos, y_pos, lvl, current_score, save_name, walkthrough_number):
+    def save_game(self, x_pos, y_pos, lvl, current_score, save_name, walkthrough_number, enemies):
         conn = sqlite3.connect(self.db_path)
         cur = conn.cursor()
         try:
@@ -42,6 +54,16 @@ class DatabaseManager:
             VALUES (?, ?, ?, ?, ?, ?)
             ''', (x_pos, y_pos, lvl, current_score, save_name, walkthrough_number))
             conn.commit()
+
+            # Сохранение состояния врагов
+            for enemy in enemies:
+                cur.execute('''
+                INSERT INTO enemy_data (enemy_type, defeated, position_x, position_y, save_name)
+                VALUES (?, ?, ?, ?, ?)
+                ''', (enemy['type'], enemy['defeated'], enemy['x'], enemy['y'], save_name))
+
+            conn.commit()
+
         except sqlite3.IntegrityError:
             pass
         finally:
@@ -62,29 +84,39 @@ class DatabaseManager:
 
         cur.execute("SELECT * FROM player_data WHERE save_name = ?", (save_name,))
         save_data = cur.fetchone()
+
+        # Получение данных о врагах
+        cur.execute("SELECT enemy_type, defeated, position_x, position_y FROM enemy_data WHERE save_name = ?",
+                    (save_name,))
+        enemies_data = cur.fetchall()
+
+        enemies = [{'type': enemy[0], 'defeated': enemy[1], 'x': enemy[2], 'y': enemy[3]} for enemy in enemies_data]
+
         conn.close()
 
         return {
             'id': save_data[0],
             'position_x': save_data[1],
             'position_y': save_data[2],
-            'lvl': save_data[3],
+            'level_number': save_data[3],
             'current_score': save_data[4],
             'save_name': save_data[5],
-            'walkthrough_number': save_data[6]
+            'walkthrough_number': save_data[6],
+            'enemies': enemies
         }
 
     def update_score(self, walkthrough_number, score):
         conn = sqlite3.connect(self.db_path)
         cur = conn.cursor()
 
-        cur.execute('SELECT * FROM game_scores WHERE order_number = ?', (walkthrough_number,))
+        cur.execute('SELECT * FROM game_scores WHERE walkthrough_number = ?', (walkthrough_number,))
         result = cur.fetchone()
 
         if result:
-            cur.execute('UPDATE game_scores SET score = ? WHERE order_number = ?', (score, walkthrough_number))
+            cur.execute('UPDATE game_scores SET score = ? WHERE walkthrough_number = ?', (score, walkthrough_number))
         else:
-            cur.execute('INSERT INTO game_scores (order_number, score) VALUES (?, ?)', (walkthrough_number, score))
+            cur.execute('INSERT INTO game_scores (walkthrough_number, score) VALUES (?, ?)',
+                        (walkthrough_number, score))
 
         conn.commit()
         conn.close()
@@ -95,7 +127,7 @@ class DatabaseManager:
         cur.execute("SELECT score FROM game_scores WHERE walkthrough_number = ?", (walkthrough_number,))
         score = cur.fetchone()
         conn.close()
-        return score[0]
+        return score[0] if score else None
 
     def get_all_scores(self):
         conn = sqlite3.connect(self.db_path)
@@ -110,6 +142,23 @@ class DatabaseManager:
         conn = sqlite3.connect(self.db_path)
         cur = conn.cursor()
         cur.execute("DELETE FROM game_scores")
-        cur.execute("DELETE FROM player data")
+        cur.execute("DELETE FROM player_data")
+        cur.execute("DELETE FROM enemy_data")
         conn.commit()
         conn.close()
+
+    def get_save_count(self):
+        conn = sqlite3.connect(self.db_path)
+        cur = conn.cursor()
+        cur.execute("SELECT COUNT(*) FROM player_data")
+        count = cur.fetchone()[0]
+        conn.close()
+        return count
+
+    def get_save_names(self):
+        conn = sqlite3.connect(self.db_path)
+        cur = conn.cursor()
+        cur.execute("SELECT save_name FROM player_data")
+        save_names = [row[0] for row in cur.fetchall()]
+        conn.close()
+        return save_names
